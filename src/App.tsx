6fd22b8dd8021ec8bd26a4afe496b8a0c9f32a8a
barbara carvalho <barbaracarvalho@barbaras-MacBook-Pro.local>
2023-03-06 19:02:09 -0300

    feat: create background image and add star wars logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import './App.css'
+import Background from './components/Background';
 
 function App() {
 
   return (
-    <div>oi</div>
+    <>
+      <Background/>
+    </>
   )
 }
 
---
+++ src/components/Background/index.tsx
@@ -0,0 +1,15 @@
+import Styled from './styles';
+
+const Background = () => {
+  return (
+    <>
+      <Styled.Sky>
+        <Styled.Stars/>
+      </Styled.Sky>
+
+      < Styled.Logo/>
+    </>
+  )
+}
+
+export default Background

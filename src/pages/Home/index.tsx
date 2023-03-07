b0ca113605d245dc29a3861a4f74da133b77e357
barbara carvalho <barbaracarvalho@barbaras-MacBook-Pro.local>
2023-03-07 17:54:56 -0300

    chore: add axios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import Search from '../../components/Search';
+import { api } from '../../lib/axios';
 import Styled from './styles';
 
 const Home = () => {
+  const [cast, setCast] = useState<string[]>([])
+   
+  useEffect(() => {
+    console.log('entrou')
+    api.get('/people').then((response) => {
+      console.log('response', response.data.data)
+
+      setCast(response.data.data)
+    })
+  }, [])
+
   return (
     <Styled.HomeContainer>
       <Styled.Stars/>
@@ -13,6 +26,10 @@ const Home = () => {
           <Search />
         </Styled.SearchContainer>
       </Styled.Header>
+
+      {cast.length > 0 && cast.map(person => (
+        <p>{person}</p>
+      ))}
 
     </Styled.HomeContainer>
   )
